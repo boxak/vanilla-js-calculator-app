1955190b46ae8cf805143af7143a7ed8292ceb15
boxak <boxak@naver.com>
2023-05-13 12:48:00 +0900

    2023-05-13 대부분 기능 완성

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+/* eslint-disable require-jsdoc */
 window.onload = function() {
-  let inputDom = document.getElementById('prompt');
-  let buttonDoms = document.querySelectorAll('button');
+  const inputDom = document.getElementById('prompt');
+  const buttonDoms = document.querySelectorAll('button');
+  const saveDom = document.getElementById('save');
 
   let prevNumber = NaN;
   let operator = '';
@@ -9,83 +11,171 @@ window.onload = function() {
   // input prompt 이벤트 추가
   inputDom.addEventListener('change', (e) => {
     let string = e.target.value;
-    
-    string = string.replace(/[^0-9]/g,''); //숫자 체크
-    inputDom.value = parseInt(string==Nan ? '' : string);
 
-    console.log(string);
+    string = string.replace(/[^0-9.-]/g, ''); // 숫자 체크
+
+    inputDom.value = string;
   });
 
   // 각 버튼마다 이벤트 추가
-  buttonDoms.forEach(dom => {
+  buttonDoms.forEach((dom) => {
     dom.addEventListener('click', onClickButtonHandler);
   });
 
   function onClickButtonHandler(e) {
-    let string = e.target.innerText;
+    const string = e.target.innerText;
     console.log(string);
-    if (parseInt(string)>=0 && parseInt(string)<=9) {
+    if ((parseInt(string)>=0 && parseInt(string)<=9) || string==='.') {
       inputPrompt(string);
-    } else if ("÷×+-".indexOf(string) > -1) {
+    } else if ('÷×+-'.indexOf(string) > -1) {
       saveNumberAndOperator(string);
-    } else if (string === "=") {
+    } else if (string === '=') {
       displayResult();
+    } else if (string === 'del') {
+      deleteString();
+    } else if (string === '±') {
+      changeSign();
+    } else if (string === 'x²') {
+      square();
+    } else if (string === '√x') {
+      sqrt();
+    } else if (string === 'CE') {
+      inputReset();
+    } else if (string === 'C') {
+      allReset();
+    } else if (string === '1/x') {
+      getReciprocal();
     }
   }
 
   // prompt에 number를 뒤에다 추가하는 함수
   function inputPrompt(number) {
-    let input = inputDom.value;
+    const input = inputDom.value;
 
-    inputDom.value = parseInt((""+input) + number);
+    if (input.length >= 2 && input[input.length-1] === '.' &&
+     number === '.') {
+      return;
+    }
+
+    let result = parseFloat((''+input) + number);
+
+    if (number === '.') {
+      result = result + '.';
+    }
+
+    inputDom.value = result;
   }
 
   // prompt에 입력한 숫자랑 연산자를 저장하는 함수
-  function saveNumberAndOperator(temp_operator) {
-    let num1 = parseInt(prevNumber);
-    let num2 = parseInt(inputDom.value);
+  function saveNumberAndOperator(tempOperator) {
+    let num1 = parseFloat(prevNumber);
+    const num2 = parseFloat(inputDom.value);
 
-    if (num1 !== NaN && operator.length > 0) {
+    if (!isNaN(num1) && operator.length > 0) {
       num1 = getAnswer(num1, num2, operator);
     } else {
       num1 = num2;
-      operator = temp_operator;
+      operator = tempOperator;
     }
 
     inputDom.value = '';
-    operator = temp_operator;
+    operator = tempOperator;
     prevNumber = num1;
 
-    console.log("prevNumber : " + prevNumber);
-
+    saveDom.innerText = prevNumber + operator;
   }
 
+  // = 연산자를 클릭하였을 때, input창에 결과 표시하고
+  // save 창의 display 정보를 지운다.
   function displayResult() {
-    let num1 = parseInt(prevNumber);
-    let num2 = parseInt(inputDom.value);
-    let temp_operator = operator;
+    const num1 = parseFloat(prevNumber);
+    const num2 = parseFloat(inputDom.value);
+    const tempOperator = operator;
 
-    let answer = getAnswer(num1, num2, temp_operator);
+    const answer = getAnswer(num1, num2, tempOperator);
 
     inputDom.value = answer;
+    saveDom.innerText = '';
   }
 
+  // 연산 결과 반환
   function getAnswer(num1, num2, oper) {
-    switch(oper) {
-      case '+' :
+    switch (oper) {
+      case '+':
         num1 = num1 + num2;
         break;
-      case '-' :
+      case '-':
         num1 = num1 - num2;
         break;
-      case '×' :
+      case '×':
         num1 = num1 * num2;
         break;
-      case '÷' :
+      case '÷':
+        if (num2 === 0) {
+          arithmeticException();
+        }
         num1 = num1 / num2;
         break;
     }
 
     return num1;
   }
-} 
+
+  function deleteString() {
+    let string = inputDom.value;
+    if (string.length > 0) {
+      string = string.substring(0, string.length-1);
+      inputDom.value = string;
+    }
+  }
+
+  function changeSign() {
+    const string = inputDom.value;
+    const number = parseFloat(string);
+
+    inputDom.value = -number;
+  }
+
+  function square() {
+    const string = inputDom.value;
+    const number = parseFloat(string);
+
+    inputDom.value = number * number;
+  }
+
+  function sqrt() {
+    const string = inputDom.value;
+    const number = parseFloat(string);
+
+    inputDom.value = Math.sqrt(number);
+  }
+
+  function inputReset() {
+    inputDom.value = '';
+  }
+
+  function allReset() {
+    inputDom.value = '';
+    saveDom.innerText = '';
+    operator = '';
+    prevNumber = '';
+  }
+
+  function getReciprocal() {
+    const string = inputDom.value;
+    const number = parseFloat(string);
+
+    if (number === 0) {
+      return;
+    }
+
+    inputDom.value = 1/number;
+  }
+
+  function arithmeticException() {
+    allReset();
+    inputDom.value = 'not divisible by zero';
+    // eslint-disable-next-line no-throw-literal
+    throw 'not divisible by zero';
+  }
+};
